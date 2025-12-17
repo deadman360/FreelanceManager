74a48d736054081ae8789fd57c786db7d23dfdd4
deadman360 <vmartinho.kfg@gmail.com>
2025-12-17 15:23:03 -0300

    created-database-migrations
    created all schemas for database contained in docs/ERD.md

---
+++ app/Http/Controllers/CredentialController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCredentialRequest;
+use App\Http\Requests\UpdateCredentialRequest;
+use App\Models\Credential;
+
+class CredentialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCredentialRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Credential $credential)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Credential $credential)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCredentialRequest $request, Credential $credential)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Credential $credential)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_12_17_173749_create_invoices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->string('invoice_number');
+            $table->decimal('amount_gross');
+            $table->decimal('platform_fee');
+            $table->decimal('amount_net');
+            $table->enum('status', ['draft','sent','paid']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
